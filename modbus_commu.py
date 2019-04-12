f6279624e59323c569ce65a56ffa71c30b1a0789
Changyong <e7217@naver.com>
2019-04-12 23:53:32 +0900

    init autonics_counter project

--- modbus_commu.py
+++ modbus_commu.py
@@ -3,10 +3,41 @@ from pymodbus.client.sync import ModbusSerialClient as modclient
 from pymodbus.register_read_message import ReadInputRegistersRequest
 from pymodbus.register_read_message import ReadHoldingRegistersRequest
 
+from time import sleep
+import sys
+import io
+import pymssql
+import time
+import datetime
+
 # import logging
 # logging.basicConfig()
 # log = logging.getLogger()
 # log.setLevel(logging.DEBUG)
+
+# ------------------------ configuration using dict -------------------------------------
+
+def confEnv(filename):
+    ## (190401) check for ':' exist.
+    def isport(dict_):
+        if dict_['port'] != '':
+            dict_['server'] = dict_['server'] + ':' + dict_['port']
+        return dict_
+
+    dict = {'port': ''}
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            comp = line.split('=')
+            for i in range(len(comp)):
+                comp[i] = comp[i].strip()
+            if (comp[0] == "server") and (comp[1].find(":") > 0):
+                dict[comp[0]], dict['port'] = (comp[1].split(':'))[0], (comp[1].split(':'))[1]
+            else:
+                dict[comp[0]] = comp[1]
+    return isport(dict)
+
+# --------------------------------------------------------------------------------------
 
 # modbus : ('coil' / 'holding' / 'analog')
 def writeToCounter(modbus, address, value):
@@ -37,13 +68,13 @@ def readFromCounter(modbus, address):
 
     if modbus == 'coil' :
         valuecheck = client1.read_coils(address, 1, unit=0x01)
-        print valuecheck.bits[0]
+        print 'value : ', valuecheck.bits[0]
     elif modbus == 'holding' :
         valuecheck = client1.read_holding_registers(address, 1, unit=0x01)
-        print valuecheck.registers
+        print 'value : ', valuecheck.registers
     elif modbus == 'analog':
         valuecheck = client1.read_input_registers(address, 1, unit=0x01)
-        print valuecheck.registers
+        print 'value : ', valuecheck.registers
 
     # print type(valuecheck)
 
@@ -51,17 +82,96 @@ def readFromCounter(modbus, address):
 
     return valuecheck
 
-# ------------------------ setting environments -------------------------------------
+def do(value):
+    try:
+        conn = pymssql.connect(envList['server'], envList['user'], envList['password'], envList['database'], timeout=3)
+        time.sleep(0.01)
+        print 'db_step_1'
+        cursor = conn.cursor()
+        time.sleep(0.01)
+        print 'db_step_2'
+        cursor.callproc('usp_xngat11t_i02', [None, mchcd, 'D', value])
+        time.sleep(0.01)
+        cursor.callproc('usp_xnwrk_u', [mchcd])
+        time.sleep(0.01)
+        # works.
+        print 'db_step_3-1'
+        cursor.callproc('usp_xngat11t_i02', [None, mchcd, 'S', 0])
+        time.sleep(0.01)
+        print 'db_step_3-2'
+        # g1.reset_()
+        writeToCounter('coil', resetAddress, forResetValue)
+        conn.commit()
+        time.sleep(0.01)
+        print 'db_step_4'
+        conn.close()
+        time.sleep(0.01)
+        print 'db_step_5 connection closed'
 
+    except:
+        print 'retry'
+        do(value)
+
+def input(value):
+    try:
+        conn = pymssql.connect(envList['server'], envList['user'], envList['password'], envList['database'], timeout = 3)
+        time.sleep(0.01)
+        print 'db_step_1'
+        cursor = conn.cursor()
+        time.sleep(0.01)
+        print 'db_step_2'
+        # works.
+        cursor.callproc('usp_xngat11t_i02', [None, mchcd, 'D', value])
+        time.sleep(0.01)
+        print 'db_step_3'
+        conn.commit()
+        time.sleep(0.01)
+        print 'db_step_4'
+        conn.close()
+        time.sleep(0.01)
+        print 'db_step_5 connection closed'
+
+    except :
+        print 'retry'
+        input()
+# ------------------------ setting environments ----------------------------------------
+
+# Configure Count and Reset Protocol
 client1 = modclient(method='rtu', port='com9', stopbits=2, bytesize=8, baudrate=9600)
-
 countValueAddress = 1003 # modbus : 'analog'
 resetAddress = 0 # modbus : 'coil'
 forResetValue = 1
 
-# -----------------------------------------------------------------------------------
+# Configuration from an environments file.
+fc_path= './env/connect_env.txt'
+envList = confEnv(fc_path)
+server = envList['server']
+user = envList['user']
+password = envList['password']
+database = envList['database']
+mchcd = envList['machine']
+
+# resetProtocol : 01 05 00 00 FF 00 8C 3A'
+# --------------------------------------------------------------------------------------
 
 # protocol for reset
-writeToCounter('coil', resetAddress, forResetValue)
+# writeToCounter('coil', resetAddress, forResetValue)
 # readFromCounter('analog', countValueAddress)
 # readFromCounter('analog', 1006)
+
+#todo: inputdata
+
+while (True) :
+    countValue = readFromCounter('analog', countValueAddress).registers
+    time.sleep(10)
+
+    # todo: reset countValue at 8 a.m
+    resettime = datetime.datetime.now().time()
+    if resettime > datetime.time(07,54) and resettime < datetime.time(07,55):
+        do(countValue)
+    else:
+        input(countValue)
+
+
+
+#todo: check response.
